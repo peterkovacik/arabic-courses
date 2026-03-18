9c0853a0e5f09f05dbb2112635f230b11ab05567
Omar <peter.a.kovacik@gmail.com>
2026-03-18 04:35:33 +0000

    Add welcome page to fix Clerk session bounce

---
+++ pages/welcome.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="welcome">
+    <p>Loading your courses...</p>
+  </div>
+</template>
+
+<script setup>
+onMounted(() => {
+  // Give Clerk 1.5 seconds to fully establish the session
+  setTimeout(() => {
+    window.location.href = '/courses'
+  }, 1500)
+})
+</script>
+
+<style scoped>
+.welcome {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  min-height: 80vh;
+  font-family: 'Cormorant Garamond', serif;
+  font-style: italic;
+  color: var(--slate);
+  font-size: 1.2rem;
+}
+</style>
