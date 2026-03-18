a10c28e4af17404ed17cbfadbd3d288c37075b41
Omar <peter.a.kovacik@gmail.com>
2026-03-18 03:21:37 +0000

    update sign-in

---
+++ pages/sign-in/factor-two.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="signin-page">
+    <SignIn />
+  </div>
+</template>
+
+<style scoped>
+.signin-page {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  min-height: 80vh;
+  background: var(--pure);
+}
+</style>
